971b6640f5951c22513b40b23f7689e57d6433d9
Taras Halynskyi <thalynskyi@codevision.com.ua>
2017-09-25 11:18:50 +0300

    fix headers

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 header( 'Location: /index.html' );
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 ?>
